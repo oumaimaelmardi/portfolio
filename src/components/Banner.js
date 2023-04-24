90cd76eea9071738bd397ee96266c748c90b2a70
Oumaima EL MARDI <82413301+oumaimaelmardi@users.noreply.github.com>
2023-04-24 13:02:27 +0200

    second commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,8 @@ import { Card } from "primereact/card";
 import { Menu } from "primereact/menu";
 import { Toast } from "primereact/toast";
 import { Image } from "primereact/image";
-import admin from "./image/adminfinal.png";
+import admin from "./image/ouma.png";
+
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 
 //core
@@ -81,22 +82,12 @@ function Banner() {
           </a>
         </div>
         <p>
-          Ingénieur d'État en Informatique diplômé de l'ENSIAS en 2009 et
-          titulaire d'un doctorat en informatique obtenu à l'Université Cadi
-          Ayyad en 2017, est également professeur en informatique à l'École
-          Nationale des Sciences Appliquées de l'Université Chouaib Doukkali, El
-          Jadida, Maroc. Ses domaines de recherche englobent le développement
-          d'outils d'automatisation dans les logiciels embarqués, la
-          modélisation et la conception de logiciels, la conception de
-          métamodèles, la transformation de modèles, les méthodes de
-          vérification et de validation de modèles, ainsi que l'application du
-          machine learning et du deep learning dans plusieurs domaines. En tant
-          que concepteur et formateur spécialisé dans les technologies JAVA, il
-          met en avant les outils et solutions Open Source pour le développement
-          de projets Internet et mobile. Il dispense des cours sur la
-          plate-forme Java Enterprise Edition (JEE), la programmation mobile, la
-          programmation web, la programmation multiplateforme, l'architecture
-          des applications d'entreprises (JEE), UML et les bases de données.
+          Futur ingénieur en informatique et réseaux, actuellement en quatrième
+          année à l'EMSI (Ecole Marocaine des Sciences de l'Ingénieur). je suis
+          une personne déterminée, créative, a une grande capacité d'analyse.
+          Par rapport au savoir faire, grâce aux stage et formations en ligne
+          que j'ai eu la chance de suivre, j'ai pu approfondir mes connaissances
+          et compétences dans Spring boot, Angular, JavaScript, Django, React.
         </p>
       </div>
     </div>
